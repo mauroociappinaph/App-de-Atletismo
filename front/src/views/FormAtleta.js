aeeb97396652d6f8108cc11af0ae31e5266743e5
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-10-25 10:53:17 -0300

    Instalación Tailwind

--- front/src/views/FormAtleta.js
+++ front/src/views/FormAtleta.js
@@ -1,10 +1,47 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
-export default function FormAtleta() {
+const FormAtleta = () => {
+  const [nombre, setNombre] = useState("");
+  const [apellido, setApellido] = useState("");
+  const [edad, setEdad] = useState("");
+  const [deporte, setDeporte] = useState("");
+
+  const handleSubmit = () => {
+    // Aquí se enviaría el formulario al servidor
+  };
+
   return (
-    <View>
-      <Text>FormAtleta</Text>
-    </View>
-  );
-}
+    <View style={tw`mt-10 flex`}>
+    <Text style={tw`text-lg font-bold mb-5`}>Crear atleta</Text>
+    <TextInput
+      style={tw`w-full h-12 px-4 py-2 border border-gray-300 rounded-md mb-5`}
+      placeholder="Nombre"
+      value={nombre}
+      onChangeText={setNombre}
+    />
+    <TextInput
+      style={tw`w-full h-12 px-4 py-2 border border-gray-300 rounded-md mb-5`}
+      placeholder="Apellido"
+      value={apellido}
+      onChangeText={setApellido}
+    />
+    <TextInput
+      style={tw`w-full h-12 px-4 py-2 border border-gray-300 rounded-md mb-5`}
+      placeholder="Edad"
+      value={edad}
+      onChangeText={setEdad}
+    />
+    <TextInput
+      style={tw`w-full h-12 px-4 py-2 border border-gray-300 rounded-md mb-5`}
+      placeholder="Deporte"
+      value={deporte}
+      onChangeText={setDeporte}
+    />
+    <Button title="Crear atleta" onPress={handleSubmit} style={tw`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`} />
+  </View>
+);
+};
+
+export default FormAtleta;
--- front/src/views/Landing.js
+++ front/src/views/Landing.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
+
+
 export default Landing = ({ navigation }) => {
   return (
-    <View style={styles.container}>
+    <View className="bg-blue-500 h-full flex items-center justify-center">
       <FontAwesome5 name="running" size={200} color="white" />
-      <Text style={styles.text}>Atletismo</Text>
+      <Text className="text-white text-4xl mt-4">Atletismo</Text>
       <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("FormAtleta")}>
-        <Text>Crear atleta</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
-        <Text>Login</Text>
+        onPress={() => navigation.navigate("FormAtleta")}
+        className="bg-green-500 p-3 rounded-md mt-4"
+      >
+        <Text className="text-white">Crear atleta</Text>
+        <Text className="text-white">Crear competición</Text>
       </TouchableOpacity>
     </View>
   );
 };
-
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -32,4 +33,11 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     margin: 20,
   },
-});
+}); */
+
+//STUB - Botón de login
+
+  /*<TouchableOpacity style={styles.button}>
+        <Text>Login</Text>
+    </TouchableOpacity> */
+
