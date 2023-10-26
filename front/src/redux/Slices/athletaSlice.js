afc5e7c29b63e010e6e2e492e0fcafe2e32175ff
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-10-26 14:02:04 -0300

    React Toolkit axios

--- front/src/redux/Slices/athletaSlice.js
+++ front/src/redux/Slices/athletaSlice.js
@@ -1,10 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const axiosAtletas = createAsyncThunk(
+  "atletas/axiosAtletas",
+  async () => {
+    const backendURL = "http://localhost:4000/atletas"; 
+    console.log("Realizando solicitud a:", backendURL);  
+    const response = await axios.get(backendURL); 
+    return response.data;
+  }
+);
 
 const initialState = {
-  atletas: 1,
+  atletas: [],
 };
 
-export default athletaSlice = createSlice({
+const atletasSlice = createSlice({
   name: "athletas",
   initialState,
   reducers: {
@@ -12,6 +23,13 @@ export default athletaSlice = createSlice({
       state.atletas = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(axiosAtletas.fulfilled, (state, action) => {
+        state.atletas = action.payload;
+      });
+  },
 });
 
 export const { setAtletas } = atletasSlice.actions;
+export default atletasSlice.reducer;
