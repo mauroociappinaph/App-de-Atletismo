db43661fd5f68f5a1a031ceb60eb76d95fa37b67
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-10-31 12:48:34 -0500

    Atletas cards

--- front/src/views/AllAtletas.js
+++ front/src/views/AllAtletas.js
@@ -1,12 +1,40 @@
-import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import CardAtleta from "../components/Atletas/CardAtleta";
 
+const atletas = [
+  {
+    id: 1,
+    nombre: "Patricio",
+    nacimiento: "09/06/2001",
+    nacionalidad: "Fondo Bikini",
+    sexo: "Estrella",
+  },
+  {
+    id: 2,
+    nombre: "Arenita",
+    nacimiento: "07/06/2001",
+    nacionalidad: "Fondo Bikini",
+    sexo: "Ardilla",
+  },
+  {
+    id: 3,
+    nombre: "Bob",
+    nacimiento: "05/06/2001",
+    nacionalidad: "Fondo Bikini",
+    sexo: "Esponja",
+  },
+];
 
 const AllAtletas = () => {
   return (
     <View style={styles.container}>
-      <Text>  
-        All Atletas
-        </Text>
+      <Text style={styles.title}>All Atletas</Text>
+      <FlatList
+        data={atletas}
+        renderItem={({ item }) => <CardAtleta item={item} />}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 };
@@ -15,20 +43,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: "#e5e5e5",
+    backgroundColor: "#ffff",
   },
   title: {
-    fontSize: 20,
+    fontSize: 50,
     fontWeight: "bold",
     marginBottom: 16,
-  },
-  atletaItem: {
-    backgroundColor: "#fff",
-    padding: 16,
-    marginBottom: 16,
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: "#ccc",
+    color: "#717B94",
   },
 });
 
