3d83b40117b881ca1d84e74c70379288f0e3d589
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-11-03 11:10:58 -0300

    Rutas de User

--- Back/routes/userRoutes.js
+++ Back/routes/userRoutes.js
@@ -1,3 +1,12 @@
 const { Router } = require("express");
-const router = express.Router();
+const router = Router();
 
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+router.get("/login", (req, res) => {
+    res.send("Login");
+});
+
+module.exports = router
