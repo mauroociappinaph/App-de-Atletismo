d2bc3ec154c9deca0c173672cb5b2882a8d01b96
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-11-05 12:23:22 -0300

    Clean and Refactor

--- Back/controllers/atletasController.js
+++ Back/controllers/atletasController.js
@@ -34,29 +34,22 @@ const createAtleta = async (req, res) => {
   }
 };
 
-
-// NOTE - Controlador para obtener un atleta por su ID
+//NOTE - Controlador para obtener un atleta por su ID
 const getAtletaById = async (req, res) => {
   try {
     const atleta = await Atleta.findById(req.params.id);
-    if (!atleta) {
-      return res.status(404).json({ message: "Atleta no encontrado" });
-    }
-    res.json(atleta);
+    if (!atleta) return res.status(404).json({ message: "Atleta no encontrado" });
+    return res.json(atleta);
   } catch (error) {
-    res.status(500).json({ message: "Error al obtener el atleta" });
+    return res.status(500).json({ message: "Error al obtener el atleta" });
   }
 };
 
 // NOTE - Actualizar un atleta por su ID
 const updateAtleta = async (req, res) => {
   try {
-    const atleta = await Atleta.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (!atleta) {
-      return res.status(404).json({ message: "Atleta no encontrado" });
-    }
+    const atleta = await Atleta.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!atleta) return res.status(404).json({ message: "Atleta no encontrado" });
     res.json(atleta);
   } catch (error) {
     res.status(500).json({ message: "Error al actualizar el atleta" });
