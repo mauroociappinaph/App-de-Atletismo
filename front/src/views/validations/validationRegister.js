6ed848c6b1d932c973a74a73127780fac9ac477c
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-11-09 10:55:04 -0500

    registro post

--- front/src/views/validations/validationRegister.js
+++ front/src/views/validations/validationRegister.js
@@ -1,21 +1,11 @@
-/* function isAdult(birthdate) {
-    const currentDate = new Date();
-    const birthDate = new Date(birthdate);
-    const timeDiff = currentDate.getTime() - birthDate.getTime();
-    const age = Math.floor(timeDiff / (1000 * 60 * 60 * 24 * 365.25));
-    return age >= 18;
-  } */
-
-const validation = (form) => {
+export const validation = (form) => {
   const errors = {};
-  if (form.fullName && !/^[A-Z][a-zA-Z ]+$/.test(form.fullName))
-    errors.fullName = "Nombre inválido";
+  if (form.nombre && !/^[A-Z][a-zA-Z ]+$/.test(form.nombre))
+    errors.nombre = "Nombre inválido";
+
   if (form.email && !/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(form.email))
     errors.email = "Formato inválido";
 
-  if (!form.sex || form.sex === "Seleccionar") errors.sex = "Escoge un sexo";
-  /*  if (form.birthdate && !isAdult(form.birthdate))
-      errors.birthdate = "Must be 18 years or older"; */
   if (
     form.password &&
     !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(form.password)
@@ -23,9 +13,20 @@ const validation = (form) => {
     errors.password =
       "La contraseña debe contener al menos 8 caracteres, una letra minúscula, una letra mayúscula y un número";
 
-  if (form.confirmPassword && form.password !== form.confirmPassword)
-    errors.confirmPassword = "Las constraseñas no coinciden";
+  if (form.repetirPassword && form.password !== form.repetirPassword)
+    errors.repetirPassword = "Las constraseñas no coinciden";
   return errors;
 };
 
-export default validation;
+export const hasErrorsOrEmptyFields = () => {
+  return (
+    error.nombre ||
+    error.email ||
+    error.password ||
+    error.repetirPassword ||
+    !form.nombre ||
+    !form.email ||
+    !form.password ||
+    !form.repetirPassword
+  );
+};
